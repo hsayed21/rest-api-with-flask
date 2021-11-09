21471b24aa59f5543c42c0e5bbe28fa777ae296a
hsayedhamed <hsayedhamed1@gmail.com>
2021-11-09 02:10:34 +0200

    Update [To Start With Google Tag Manager]

--- app.py
+++ app.py
@@ -7,55 +7,61 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-from base import Movies, db
+
+from base import data, db
 db.init_app(app)
 app.app_context().push()
 db.create_all()
 
-class Movies_List(Resource):
+class data_List(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('director', type=str, required=False, help='Director of the movie')
-    parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
-    parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
+    parser.add_argument('page_url', type=str, required=True, help='page_url of the data_Item')
+    parser.add_argument('page_view_count', type=int, required=True, help='page_view_count of the data_Item')
+    parser.add_argument('last_seen', type=str, required=True, help='last_seen of the data_Item')
+    parser.add_argument('timestamp', action='append', required=True, help='timestamp list of the data_Item')
     
-    def get(self, movie):
-        item = Movies.find_by_title(movie)
+    def get(self, data_Item_Id):
+        item = data.find_by_id(data_Item_Id)
         if item:
             return item.json()
-        return {'Message': 'Movie is not found'}
+        return {'Message': 'ID = {} is not found'.format(data_Item_Id)}
     
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
+    def post(self, data_Item_Id):
+        if data.find_by_id(data_Item_Id):
+            return {' Message': 'Data Item with ID = {} already exists'.format(data_Item_Id)}
+        args = data_List.parser.parse_args()
+        item = data(data_Item_Id, args['page_url'], args['page_view_count'], args['last_seen'], args['timestamp'])
         item.save_to()
         return item.json()
         
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
+    def put(self, data_Item_Id):
+        args = data_List.parser.parse_args()
+        print(args.timestamp)
+        item = data.find_by_id(data_Item_Id)
         if item:
-            item.collection = args['collection']
+            item.page_url = args['page_url']
+            item.page_view_count = args['page_view_count']
+            item.last_seen = args['last_seen']
+            item.timestamp = args['timestamp']
             item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
+            return {'dataItem': item.json()}
+        item = data(data_Item_Id, args['page_url'], args['page_view_count'], args['last_seen'], args['timestamp'])
         item.save_to()
         return item.json()
             
-    def delete(self, movie):
-        item  = Movies.find_by_title(movie)
+    def delete(self, data_Item_Id):
+        item  = data.find_by_id(data_Item_Id)
         if item:
-            item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
-        return {'Message': '{} is already not on the list'.format()}
+            item.delete()
+            return {'Message': 'Data Item with ID = {} has been deleted from records'.format(data_Item_Id)}
+        return {'Message': 'ID = {} is already not on the list'.format(data_Item_Id)}
     
-class All_Movies(Resource):
+class All_data(Resource):
     def get(self):
-        return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
+        return {'Data': list(map(lambda x: x.json(), data.query.all()))}
     
-api.add_resource(All_Movies, '/')
-api.add_resource(Movies_List, '/<string:movie>')
+api.add_resource(All_data, '/')
+api.add_resource(data_List, '/<string:data_Item_Id>')
 
 if __name__=='__main__':
     
--- base.py
+++ base.py
@@ -1,31 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Movies(db.Model):
+class data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), unique=True, nullable=False)
-    director = db.Column(db.String(30), unique=False, nullable=False)
-    genre = db.Column(db.String(30), unique=False, nullable=False)
-    collection = db.Column(db.Integer, unique=False, nullable=False)
+    page_url = db.Column(db.String(60), unique=False, nullable=False)
+    page_view_count = db.Column(db.Integer, unique=False, nullable=False)
+    last_seen = db.Column(db.String(60), unique=False, nullable=False)
+    timestamp = db.Column(db.PickleType, unique=False, nullable=False)
     
-    def __init__(self, title, director, genre, collection):
-        self.title = title
-        self.director = director
-        self.genre = genre
-        self.collection = collection
+    def __init__(self, id, page_url, page_view_count, last_seen, timestamp):
+        self.id = id
+        self.page_url = page_url
+        self.page_view_count = page_view_count
+        self.last_seen = last_seen
+        self.timestamp = timestamp
         
     def json(self):
-        return {'Title': self.title, 'Director': self.director, 'Genre': self.genre, 'Collection': self.collection}
+        return {'id':self.id, 'page_url': self.page_url, 'page_view_count': self.page_view_count, 'last_seen': self.last_seen, 'timestamp': self.timestamp}
     
     @classmethod
-    def find_by_title(cls, title):
-        return cls.query.filter_by(title=title).first()
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
     
     def save_to(self):
         db.session.add(self)
         db.session.commit()
         
-    def delete_(self):
+    def delete(self):
         db.session.delete(self)
         db.session.commit()
         
